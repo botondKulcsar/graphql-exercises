f729d48f717c243c5150c2ecce720a9977871550
Botond Kulcsar <bkulcsar@gmx.com>
2021-11-05 17:08:02 +0200

    exercise 8.16

--- index.js
+++ index.js
@@ -1,103 +1,27 @@
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, AuthenticationError, gql } = require('apollo-server')
 // const { v1: uuid } = require('uuid')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('connected to MongoDB'))
     .catch(err => console.err('connection to DB failed with error: ', err.message))
 
-let authors = [
-    {
-        name: 'Robert Martin',
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952,
-    },
-    {
-        name: 'Martin Fowler',
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963
-    },
-    {
-        name: 'Fyodor Dostoevsky',
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821
-    },
-    {
-        name: 'Joshua Kerievsky', // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: 'Sandi Metz', // birthyear not known
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-]
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's name in the context of the book instead of the author's id
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
-let books = [
-    {
-        title: 'Clean Code',
-        published: 2008,
-        author: 'Robert Martin',
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Agile software development',
-        published: 2002,
-        author: 'Robert Martin',
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ['agile', 'patterns', 'design']
-    },
-    {
-        title: 'Refactoring, edition 2',
-        published: 2018,
-        author: 'Martin Fowler',
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Refactoring to patterns',
-        published: 2008,
-        author: 'Joshua Kerievsky',
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'patterns']
-    },
-    {
-        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-        published: 2012,
-        author: 'Sandi Metz',
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'design']
-    },
-    {
-        title: 'Crime and punishment',
-        published: 1866,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'crime']
-    },
-    {
-        title: 'The Demon ',
-        published: 1872,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'revolution']
-    },
-]
 
 const typeDefs = gql`
+  type User {
+      username: String!
+      favoriteGenre: String!
+      id: ID!
+  }
+  type Token {
+      value: String!
+  }
   type Book {
       title: String!
       published: Int!
@@ -116,8 +40,17 @@ const typeDefs = gql`
       authorCount: Int!
       allBooks(author: String, genre: String): [Book!]!
       allAuthors: [Author!]!
+      me: User
   }
   type Mutation {
+      createUser(
+          username: String!
+          favoriteGenre: String!
+      ): User!
+      login(
+          username: String!
+          password: String!
+      ): Token
       addBook(
         title: String!
         author: String!
@@ -133,8 +66,9 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        bookCount: () => Book.collection.countDocuments(),
-        authorCount: () => Author.collection.countDocuments(),
+        me: (root, args, context) => context.currentUser, 
+        bookCount: (root) => Book.collection.countDocuments(),
+        authorCount: (root) => Author.collection.countDocuments(),
         allBooks: async (root, args) => {
             try {
                 const filter = {}
@@ -147,7 +81,7 @@ const resolvers = {
                 console.error(error)
             }
         },
-        allAuthors: async () => {
+        allAuthors: async (root, args) => {
             try {
                 const authors = await Author.find({})
                 for (const author of authors) {
@@ -161,7 +95,43 @@ const resolvers = {
         }
     },
     Mutation: {
-        addBook: async (root, args) => {
+        createUser: async (root, args) => {
+            try {
+                const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+                const savedUser = await user.save()
+                return savedUser
+            } catch (error) {
+                throw new UserInputError(error.message, {
+                    invalidArgs: args,
+                  })
+            }
+        },
+        login: async (root, args) => {
+            try {
+                const { username, password } = args
+                const user = await User.findOne({ username })
+                if (!user || password !== '1234') {
+                    throw new Error('wrong credentials')
+                }
+
+                const userForToken = {
+                    username: user.username,
+                    id: user._id
+                }
+
+                const token = jwt.sign(userForToken, process.env.JWT_SECRET)
+
+                return { value: token }
+            } catch (error) {
+                throw new UserInputError(error.message, {
+                    invalidArgs: args,
+                  })
+            }
+        },
+        addBook: async (root, args, { currentUser }) => {
+            if (!currentUser) {
+                throw new AuthenticationError('not authenticated')
+            }
             try {
                 let author = await Author.findOne({ name: args.author })
                 if (!author) {
@@ -176,12 +146,15 @@ const resolvers = {
             } catch (error) {
                 if (error instanceof mongoose.Error.ValidationError) {
                     throw new UserInputError(`Invalid title or author name`, {
-                        // invalidArgs: args.title || args.author,
+                        invalidArgs: args
                     })
                 }
             }
         },
-        editAuthor: async (root, args) => {
+        editAuthor: async (root, args, { currentUser }) => {
+            if (!currentUser) {
+                throw new AuthenticationError('not authenticated')
+            }
             try {
                 const author = await Author.findOneAndUpdate({ name: args.name }, { born: args.setBornTo }, { new: true })
                 if (!author) {
@@ -191,7 +164,9 @@ const resolvers = {
                 author.bookCount = bookCount
                 return author
             } catch (error) {
-                console.log(error.message)
+                throw new UserInputError(error.message, {
+                    invalidArgs: args.author
+                })
             }
 
         }
@@ -202,6 +177,17 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: async ({req}) => {
+        const auth = req ? req.headers.authorization : null
+    
+        if (auth && req.headers.authorization.toLowerCase().startsWith('bearer ')) {
+            const decodedToken = jwt.verify(
+                auth.substring(7), process.env.JWT_SECRET
+            )
+            const currentUser = await User.findById(decodedToken.id)
+            return { currentUser }
+        }
+    }
 })
 
 server.listen().then(({ url }) => {
